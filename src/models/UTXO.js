b90927a9256ac1327e975213b0de6d2fe68dbc8d
H4O8ML6E9S <95122072+H4O8ML6E9S@users.noreply.github.com>
2023-06-11 12:29:17 +0800

    Update UTXO.js

--- src/models/UTXO.js
+++ src/models/UTXO.js
@@ -1,10 +1,11 @@
 /*
  * @Author: 南宫
- * @Date: 2023-06-10 21:16:46
- * @LastEditTime: 2023-06-11 09:09:27
+ * @Date: 2023-05-18 18:20:41
+ * @LastEditTime: 2023-06-11 10:33:16
  */
 export default class UTXO {
-  constructor(amount) {
+  constructor(amount, fee = 0) {
     this.amount = amount
+    this.fee = fee
   }
 }
