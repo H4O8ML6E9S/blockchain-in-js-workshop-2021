7f58ea9351c70f03a38007ed2c22fae2d1db90db
H4O8ML6E9S <95122072+H4O8ML6E9S@users.noreply.github.com>
2023-06-11 10:29:19 +0800

    Update Transaction.js

--- src/models/Transaction.js
+++ src/models/Transaction.js
@@ -1,43 +1,19 @@
-/*
- * @Author: 南宫
- * @Date: 2023-05-21 11:03:37
- * @LastEditTime: 2023-05-31 23:01:21
- */
 import sha256 from 'crypto-js/sha256.js'
 
 class Transaction {
-  constructor(senderPublicKey, receiverPublicKey, amount) {
-    this.senderPublicKey = senderPublicKey
-    this.receiverPublicKey = receiverPublicKey
-    this.amount = amount
-    this.timestamp = new Date().getTime()
-    this.hash = this._calculateHash()
+  constructor() {
+
   }
 
   // 更新交易 hash
-  _setHash () {
-    this.hash = this._calculateHash()
+  _setHash() {
+
   }
 
   // 计算交易 hash 的摘要函数
-  _calculateHash () {
-    return sha256(this.senderPublicKey + this.receiverPublicKey + this.amount + this.timestamp).toString()
+  _calculateHash() {
+
   }
-
-  isValidTransaction () {
-    // 验证交易金额是否非负
-    if (this.amount <= 0) {
-      return false;
-    }
-
-    // 验证交易哈希是否正确
-    if (this.hash !== this._calculateHash()) {
-      return false;
-    }
-
-    return true;
-  }
-
 }
 
 export default Transaction
